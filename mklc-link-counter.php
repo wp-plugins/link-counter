8c2a0740a8a4fd33082e763b82b9cce538a04530
henryww <henryww@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-05 19:20:45 +0000

    mklc-link-counter.php update
    git-svn-id: https://plugins.svn.wordpress.org/link-counter/trunk@500704 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mklc-link-counter.php
+++ mklc-link-counter.php
@@ -3,9 +3,20 @@
 Plugin Name: Link Counter
 Plugin URI: http://www.henrywoodbury.com/link-counter/
 Description: Counts links in posts; organizes list by domain if desired
-Version: 1.0
+Version: 1.1
 Author: Henry Woodbury
 Author URI: http://www.henrywoodbury.com/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
 */
 
 // Return lower case string
@@ -20,7 +31,7 @@ function mklc_strtolower($str = '') {
 
 // Add http:// to links
 function mklc_addHTTP($link = '') {
-	if (substr($link, 0, 7) != 'http://') return 'http://' . $link;
+	if (substr($link, 0, 7) != 'http://' && substr($link, 0, 8) != 'https://') return 'http://' . $link;
 	else return $link;
 }
 
@@ -29,26 +40,30 @@ function mklc_sortLinks($k, $sort, $order) {
 	if ($sort == 'count') 
 		if ($order == 'ascending') asort($k);
 		else arsort($k);
-	elseif ($sort == 'alpha') 
-		if ($order == 'descending') krsort($l);
+	elseif ($sort == 'alpha')
+		if ($order == 'descending') krsort($k);
 		else ksort($k);
 	return $k;
 }
 
 
 // Count links. Defaults are:
-// dupes - 'yes' = All matches including duplicates, 'no' = Ignore duplicates in individual posts
+// dupes 	- 'yes' = All matches including duplicates, 'no' = Ignore duplicates in individual posts
 // output - 'all' = All domains and links, 'domain' = domains only, 'link' = links only
-// target - 'all' = All URLs, 'other' = Exclude blog URL (NEED TO DO: a single domain, or domain with * and ? wildcards);
-// sort - 'count' = Sourt by count, 'alpha' = Sort alphabetically, 'none' = no sort (will follow order of first occurence);
-// order - 'ascending' or 'descending'
-// scope - 'all' = posts and pages, 'post' = posts only, 'page' = pages only
+// target 	- 'all' = All URLs, 
+//				- 'other' = Exclude blog URL 
+//				- (NEED TO DO: a single domain, or domain with * and ? wildcards)
+// active	- 'yes' = Only active links (those attached to an href), 'no' = all URLs 
+// sort 		- 'count' = Sourt by count, 'alpha' = Sort alphabetically, 'none' = no sort (will follow order of first occurence);
+// order 	- 'ascending' or 'descending'
+// scope 	- 'all' = posts and pages, 'post' = posts only, 'page' = pages only
 // NEED TO DO: Date Range
 function mklc_cc($atts) {
 	extract(shortcode_atts(array(
 		'dupes' => 'no',
 		'output' => 'all',
 		'target' => 'other',
+		'active' => 'yes',
 		'sort' => 'alpha',
 		'order' => 'ascending',
 		'scope' => 'post'
@@ -60,13 +75,18 @@ function mklc_cc($atts) {
 	$now = gmdate("Y-m-d H:i:s",time()); 
 
 //  Define some regular expressions for matching links and domains.
-//  The first is John Gruber's 'Liberal, Accurate Regex Pattern for Matching URLs' 
-//	http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
-	$patternURL = "\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))";
-	$patternSUB = "\b(([\w-]+://?|www[.])([^\s()<>/]+))";
-	$patternWWW = "\bwww[.]";
+//  See John Gruber's 'Liberal, Accurate Regex Pattern for Matching URLs' 
+//	  http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
+// Capture all web links
+	$patternURL = "\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))";
+// Capture "active" links (those associated to a valid href
+	$patternAURL = "\bhref=(?:\"?|'?)((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))";
+// Pattern to extract subdomain from link	
+	$patternSUB = "\b(([\w-]+://?|www\d{0,3}[.])([^\s()<>/]+))";
+// Pattern to remove www from subdomain
+	$patternWWW = "\bwww\d{0,3}[.]";
 	
-//	Retrieve URL of blog and 
+//	Retrieve URL of blog 
 	$url = mklc_strtolower(get_bloginfo('url'));
 	preg_match("@$patternSUB@", $url, $match);	
 	$blogURL = $match[3];
@@ -88,6 +108,7 @@ function mklc_cc($atts) {
 
 //	Define arrays to store link, domain, post ID
 	$linkList = array();
+	$activeLinkList = array();
 	$domainList = array();
 	$idList = array();
 	$linksByDomain = array();
@@ -103,10 +124,19 @@ function mklc_cc($atts) {
 			$taggedContent = $post->post_content;
 			$taggedContent = mklc_strtolower($taggedContent);
 //	Match links
-			preg_match_all("@$patternURL@", $taggedContent, $matches, PREG_PATTERN_ORDER);
-//  Remove duplicates in same post
-			if ($dupes == 'no') $links = array_unique($matches[1]);
-			else $links = $matches[1];
+			if ($active == 'yes') {
+// Match active links
+				preg_match_all("@$patternAURL@", $taggedContent, $matches, PREG_PATTERN_ORDER);
+			} else {
+// Match all links
+				preg_match_all("@$patternURL@", $taggedContent, $matches, PREG_PATTERN_ORDER);
+			}
+// Remove duplicates in same post
+			if ($dupes == 'no') {
+				$links = array_unique($matches[1]);
+			} else {
+				$links = $matches[1];
+			}
 //	Capture subdomain of each link and add to $linkData array
 			foreach ($links as $link) {
 				preg_match("@$patternSUB@", $link, $match);
@@ -116,7 +146,7 @@ function mklc_cc($atts) {
 				$linkList[] = mklc_addHTTP($link);
 //	Add post id to list of ids
 				$idList[] = $id;
-// 	Remove 'www' form domain name, but retain it for links	
+// Remove 'www' from domain name, but retain it for links	
 				$domain = preg_replace("@$patternWWW@", '', $match[3]);
 //	Add domain to list of domains
 				$domainList[] = $domain;
@@ -125,7 +155,7 @@ function mklc_cc($atts) {
 				if ($domain != $match[3]) $domainListWWW[$domain] = 'www.';
 //	Add domain to array of domain links
 				$domainListLink[$domain] =  mklc_addHTTP($domainListWWW[$domain] . $domain);
-// 	Add link to array of links for each domain
+// Add link to array of links for each domain
 				$linksByDomain[$domain][] = mklc_addHTTP($link);
 			}
     	}
@@ -139,7 +169,7 @@ function mklc_cc($atts) {
 // 	Sort the Domains
 	$d = mklc_sortLinks($d, $sort, $order);
 // 	Sort the Links
-	$a = mklc_sortLinks($d, $sort, $order);
+	$a = mklc_sortLinks($a, $sort, $order);
 
 // Clean up linksByDomain
 	foreach ($linksByDomain as $domain => $links) {
@@ -152,7 +182,7 @@ function mklc_cc($atts) {
 // 	Print the data.
 	$mklc = '<div class="mklc">';
 
-	if ($output != 'links') {
+	if ($output != 'link') {
 		$mklc .= '<ul class="mklc-list-domains">';
 		foreach ($d as $domain => $count) {
 			$mklc .= '<li>' . $count . ' - <a href="' . $domainListLink[$domain] . '">' . $domain . '</a></li>'; 
@@ -172,7 +202,6 @@ function mklc_cc($atts) {
 		}
 		$mklc .= '</ul>';
 	}
-
 
 	$mklc .= '</ul>';
 	$mklc .= '</div>';
